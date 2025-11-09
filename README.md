7d1ee7f5408113f74436dc9e67d02af490b69b9f
heyzec <61238538+heyzec@users.noreply.github.com>
2025-11-09 12:06:46 +0800

    feat: create prototype

--- README.md
+++ README.md
@@ -1,27 +1,57 @@
-# vscode-ext
+# PBLeap
 
-<!-- Uncomment after adding repository to package.json (this key is needed for `vsce` to package extensions where README has images)
 <p align="center">
    <img src="./assets/icon.png" width="150">
 </p>
--->
 
-Sample VSCode extension with project setup for:
-- Bundling of node dependencies
-- Hot reload during development
+A Visual Studio Code extension to quickly navigate from protobuf files to generated code.
+
+## Features
+- Leap from protobuf definitions to generated code
+- Leap from generated code back to protobuf definitions
+
+(only Go supported for now)
+
+## Getting started
+After PBLeap is installed, you need to configure the mapping between protobuf files and generated files. In the settings page, search for `protoGenMapping` (or open this deeplink: `vscode://settings/pbleap.protoGenMapping`).
+
+This setting has to be edited in JSON format. The key is the protobuf filename and the value is the generated code filename. Both are relative to workspace root.
+
+```json
+"pbleap.protoGenMapping": {
+    "example.proto": "example.pb.go"
+}
+```
 
 ## Developer Guide
-Setup and install
+### Setting up
+**1. Install node dependencies**
 ```sh
 npm install
+```
+
+**2. Get required WASM binaries for parsing**
+
+This extension uses [Tree-sitter](https://github.com/tree-sitter/tree-sitter) to parse code and generate ASTs. WASM binaries need to be at `dist/tree-sitter-proto.wasm` and `dist/tree-sitter-go.wasm` before VSIX packaging.
+
+- Protobuf: https://github.com/coder3101/tree-sitter-proto
+   - Will require manually building from source with the `tree-sitter` CLI.
+- Go: https://github.com/tree-sitter/tree-sitter-go
+   - Can be obtained from GitHub Releases.
+   - Alternatively, install the NPM package and then copy from `node-modules/tree-sitter-go/tree-sitter-go.wasm`.
+
+If you have Nix, you can instead run
+```sh
+just wasm
+```
+which will build and copy the required WASM files to `dist/`.
+
+### Packaging and installing from VSIX
+```sh
 just install
 ```
 
-Install and trigger reload
-```sh
-just reinstall
-```
-
+### Developing
 Start hot-reload of extension
 ```sh
 just dev
--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,354 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as path from "path";
+
+import { Parser, Language } from "web-tree-sitter";
+import type { Node } from "web-tree-sitter";
 
 // For stacktraces to show line numbers mapped from to typescript files (even on production builds)
 require("source-map-support").install();
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "helloworld" is now active!');
+// TODO: SHouldn be dynamic
+const mapping =
+  vscode.workspace
+    .getConfiguration("untitled")
+    .get<{ [key: string]: string }>("protoGenMapping") || {};
 
-  const disposables: vscode.Disposable[] = [];
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  disposables.push(
-    vscode.commands.registerCommand("vscode-ext.helloWorld", () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from HelloWorld!");
-    })
-  );
+let protoFileDocument;
 
-  return disposables;
+function getGoFileFromProtoFile(protoPath: string): string | undefined {
+  return mapping[protoPath];
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function getProtoFileFromGoFile(goPath: string): string | undefined {
+  for (const [proto, go] of Object.entries(mapping)) {
+    if (go === goPath) {
+      return proto;
+    }
+  }
+}
+
+function nodesToLocations(
+  nodes: /*(Node | null)[]*/ any,
+  document: vscode.TextDocument
+): vscode.Location[] {
+  return nodes.map((node: any) => {
+    const startPos = new vscode.Position(
+      node.startPosition.row,
+      node.startPosition.column
+    );
+    const endPos = new vscode.Position(
+      node.endPosition.row,
+      node.endPosition.column
+    );
+    const range = new vscode.Range(startPos, endPos);
+    return new vscode.Location(document.uri, range);
+  });
+}
+
+function getGoStruct(root: /*Node*/ any, structName: string): Node | null {
+  let nodes = root.namedChildren.filter(
+    (node: any) => node.type === "type_declaration"
+  );
+  nodes = nodes.filter(
+    (node: any) =>
+      node.namedChildren[0].childForFieldName("name")?.text === structName
+  );
+  return nodes[0];
+}
+
+function getGoField(
+  root: Node,
+  structName: string,
+  fieldName: string
+): Node | null {
+  const structNode = getGoStruct(root, structName);
+  let nodes =
+    structNode?.namedChildren[0]?.childForFieldName("type")?.namedChildren[0]
+      ?.namedChildren ?? [];
+  nodes = nodes.map(
+    (node) =>
+      node?.namedChildren.find((child) => child?.type === "field_identifier") ??
+      null
+  );
+  nodes = nodes.filter((node) => node?.text === fieldName);
+  return nodes[0];
+}
+
+function getProtoStruct(root: Node, structName: string) {
+  let nodes = root.namedChildren;
+  nodes = nodes.filter((node) => node?.type === "message");
+  nodes = nodes.filter((node) => node?.child(1)?.text === structName);
+  return nodes[0];
+}
+
+function getProtoField(
+  root: Node,
+  structName: string,
+  fieldName: string
+): Node | null {
+  const structNode = getProtoStruct(root, structName);
+  let nodes = structNode?.child(2)?.namedChildren ?? [];
+  nodes = nodes.filter((node) => node?.type === "field");
+  nodes = nodes.map((node) => node?.child(2) ?? null);
+  return nodes.find((node) => node?.text === fieldName) ?? null;
+}
+
+async function highlightNodes(nodes: Node[], doc: vscode.TextDocument) {
+  // hsl(x 100% 25%)
+  // x =          0deg       90deg      180deg     270deg
+  const COLORS = ["#800000", "#408000", "#008080", "#400080"];
+  const editor = await vscode.window.showTextDocument(doc);
+  nodes.forEach((node, index) => {
+    const startPos = new vscode.Position(
+      node.startPosition.row,
+      node.startPosition.column
+    );
+    const endPos = new vscode.Position(
+      node.endPosition.row,
+      node.endPosition.column
+    );
+    const range = new vscode.Range(startPos, endPos);
+    const dec = vscode.window.createTextEditorDecorationType({
+      backgroundColor: COLORS[index % COLORS.length],
+    });
+    editor.setDecorations(dec, [range]);
+  });
+}
+
+function printNode(node: any, indent = 0) {
+  const output: string[] = [];
+  function recurse(node: any, indent = 0) {
+    const padding = "  ".repeat(indent);
+    let info = node.type;
+    if (node.namedChildCount === 0) {
+      // leaf node: show text if short
+      const text = node.text.replace(/\s+/g, " ");
+      if (text.length < 30) info += `: "${text}"`;
+    }
+    output.push(padding + info);
+
+    for (const child of node.namedChildren) {
+      recurse(child, indent + 1);
+    }
+  }
+  recurse(node, indent);
+  console.log(output.join("\n"));
+}
+
+function snakeToPascal(s: string): string {
+  return s
+    .split("_")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join("");
+}
+
+function pascalToSnake(s: string): string {
+  return s
+    .replace(/([A-Z])/g, "_$1")
+    .toLowerCase()
+    .replace(/^_/, "");
+}
+
+let protoParser: Parser;
+let goParser: Parser;
+
+async function initProtoParser(context: vscode.ExtensionContext) {
+  const parser = new Parser();
+  const Proto = await Language.load(
+    path.join(context.extensionPath, "dist", "tree-sitter-proto.wasm")
+  );
+  parser.setLanguage(Proto);
+  protoParser = parser;
+}
+
+async function initGoParser(context: vscode.ExtensionContext) {
+  const parser = new Parser();
+  const Go = await Language.load(
+    path.join(context.extensionPath, "dist", "tree-sitter-go.wasm")
+  );
+  parser.setLanguage(Go);
+  goParser = parser;
+}
+
+async function handleGoReference(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): Promise<vscode.Location[]> {
+  const tree = goParser.parse(document.getText());
+  if (!tree) {
+    return [];
+  }
+
+  const [r, c] = [position.line, position.character];
+  let node: Node | null = tree.rootNode.descendantForPosition(
+    { row: r, column: c },
+    { row: r, column: c }
+  );
+
+  if (!node || node.type !== "field_identifier") {
+    return [];
+  }
+
+  const fieldName = node.text;
+  if (!fieldName) {
+    return [];
+  }
+
+  // Assumption: We can always find struct containing field
+  node = node.parent?.parent?.parent?.parent || null;
+  if (!node || node.type !== "type_spec") {
+    return [];
+  }
+
+  const structName = node.child(0)?.text;
+  if (!structName) {
+    return [];
+  }
+
+  const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
+  if (!workspaceFolder) {
+    return []; // file is outside workspace
+  }
+  const goPath = path.relative(workspaceFolder.uri.fsPath, document.uri.fsPath);
+  const protoPath = getProtoFileFromGoFile(goPath);
+  if (!protoPath) {
+    vscode.window.showInformationMessage("No proto found for " + document.uri);
+    return [];
+  }
+
+  const protoFileUri = vscode.Uri.joinPath(workspaceFolder.uri, protoPath);
+  /* const */ protoFileDocument =
+    await vscode.workspace.openTextDocument(protoFileUri); // Make it global for debugging
+
+  const protoTree = protoParser.parse(protoFileDocument.getText());
+  if (!protoTree) {
+    return [];
+  }
+  console.log(
+    "Searching for field:",
+    pascalToSnake(fieldName),
+    "in message:",
+    structName
+  );
+
+  const found = getProtoField(
+    protoTree.rootNode,
+    structName,
+    pascalToSnake(fieldName)
+  );
+  return nodesToLocations([found], protoFileDocument);
+}
+
+async function handleProtoReference(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): Promise<vscode.Location[]> {
+  const protoTree = protoParser.parse(document.getText());
+
+  const [r, c] = [position.line, position.character];
+
+  let node = protoTree?.rootNode.descendantForPosition(
+    { row: r, column: c },
+    { row: r, column: c }
+  );
+  const identifierName = node?.text;
+  if (!node || !node.type || node.type !== "identifier" || !identifierName) {
+    vscode.window.showInformationMessage(
+      `Only supported on identifier, found ${node?.type}`
+    );
+    return [];
+  }
+
+  // Assumption: We can always find message containing identifier
+  while (node?.type !== "message") {
+    node = node?.parent;
+  }
+
+  const messageName = node?.child(1)?.text;
+  if (!messageName) {
+    return []; // file is outside workspace
+  }
+
+  const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
+  if (!workspaceFolder) {
+    return []; // file is outside workspace
+  }
+  const protoPath = path.relative(
+    workspaceFolder.uri.fsPath,
+    document.uri.fsPath
+  );
+  const goPath = getGoFileFromProtoFile(protoPath);
+  if (!goPath) {
+    vscode.window.showInformationMessage("No maing found for " + document.uri);
+    return [];
+  }
+
+  const goFileUri = vscode.Uri.joinPath(workspaceFolder.uri, goPath);
+  const goFileDocument = await vscode.workspace.openTextDocument(goFileUri);
+  console.log("Going to parse!!!!");
+  // console.log("Go file text:", goFileDocument.getText().slice(0, 200)); // Log first 200 characters
+  const goTree = goParser.parse(goFileDocument.getText());
+  if (!goTree) {
+    return [];
+  }
+
+  console.log(
+    "Searching for field:",
+    snakeToPascal(identifierName),
+    "in message:",
+    messageName
+  );
+  const found = getGoField(
+    goTree.rootNode,
+    messageName,
+    snakeToPascal(identifierName)
+  );
+  console.log("Found Go field node:", found?.toString());
+
+  return nodesToLocations([found], goFileDocument);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  Parser.init()
+    .then(() => {
+      initProtoParser(context);
+      initGoParser(context);
+    })
+    .catch((err) => {
+      console.error("Failed to initialize parsers:", err);
+    });
+
+  const disposables: vscode.Disposable[] = [];
+
+  const subscriptions = [
+    vscode.languages.registerReferenceProvider(
+      [{ language: "proto", scheme: "file" }, { pattern: "**/*.proto" }],
+      {
+        provideReferences: handleProtoReference,
+      }
+    ),
+    vscode.languages.registerDefinitionProvider(
+      [{ language: "proto", scheme: "file" }, { pattern: "**/*.proto" }],
+      {
+        provideDefinition: handleProtoReference,
+        // provideDefinition: () => []
+        // TODO: Find another way to allow ctrl click, this is technically wrong
+      }
+    ),
+    vscode.languages.registerDefinitionProvider(
+      [{ language: "go", scheme: "file" }, { pattern: "**/*.pb.go" }],
+      {
+        provideDefinition: handleGoReference,
+      }
+    ),
+  ];
+
+  disposables.push(...subscriptions);
+
+  return disposables.map((d) => ({
+    dispose: () => {
+      console.log("Disposable.dispose() called!");
+      d.dispose();
+    },
+  }));
+}
