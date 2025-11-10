8512e292fee1ee77bd3f1dedef257ab5eb698f42
heyzec <61238538+heyzec@users.noreply.github.com>
2025-11-11 00:18:01 +0800

    feat: enums, nested messages, nested enums

--- src/walkers/proto.ts
+++ src/walkers/proto.ts
@@ -1,5 +1,3 @@
-import * as vscode from "vscode";
-
 import type { Node } from "web-tree-sitter";
 
 import { Route, Walker, WalkerFactory } from "./base";
@@ -15,33 +13,100 @@ class ProtoWalkerFactory extends WalkerFactory {
   }
 }
 
+/*
+  message User {
+          ^^^^^^^^            (1) identifier -> message_name -> message
+      optional int32 id = 1;
+                     ^^       (2) identifier -> field -> message_body -> message
+  }
+
+  enum Status {
+       ^^^^^^                 (3) identifier -> enum_name -> enum
+      ACTIVE = 0;
+      ^^^^^^                  (4) identifier -> enum_field -> enum_body -> enum
+  }
+*/
+
+function walkToRoot(start: Node, route: Route) {
+  let node: Node | null = start;
+  while (node) {
+    node = node?.parent;
+    if (node?.type === "message") {
+      const messageName = node?.child(1)?.text!;
+      route!.unshift({ type: "message", name: messageName });
+    }
+  }
+}
+
+function handleCase1(node: Node): Route {
+  console.debug("ProtoWalker: Case1");
+  node = node.parent!;
+  const route: Route = [];
+  walkToRoot(node, route);
+  return route;
+}
+
+function handleCase2(node: Node): Route {
+  console.debug("ProtoWalker: Case2");
+  const fieldName = node?.text!;
+  node = node.parent!;
+
+  const route: Route = [];
+  node = node!.parent!.parent!; // type must be message (field -> message_body -> message)
+  const structName = node?.child(1)?.text!;
+  route.push({ type: "message", name: structName });
+  route.push({ type: "field", name: fieldName });
+
+  walkToRoot(node, route);
+  return route;
+}
+
+function handleCase3(node: Node): Route {
+  console.debug("ProtoWalker: Case3");
+  const enumName = node?.text!;
+  const route: Route = [];
+  route.push({ type: "enum", name: enumName });
+  node = node.parent!;
+  walkToRoot(node, route);
+  return route;
+}
+
+function handleCase4(node: Node): Route {
+  console.debug("ProtoWalker: Case4");
+  const fieldName = node?.text!;
+  node = node.parent!;
+
+  const route: Route = [];
+  node = node!.parent!.parent!; // type must be enum (enum_field -> enum_body -> enum)
+  const enumName = node?.child(1)?.text!;
+  route.push({ type: "enum", name: enumName });
+  route.push({ type: "enum_field", name: fieldName });
+
+  walkToRoot(node, route);
+  return route;
+}
+
 class ProtoWalker extends Walker {
   getRoute(thisNode: Node) {
-    let node: Node | null = thisNode;
-    const fieldName = node?.text;
-    if (!node || !node.type || node.type !== "identifier" || !fieldName) {
-      vscode.window.showInformationMessage(
-        `Only supported on identifier, found ${node?.type}`
-      );
-      return null;
-    }
-    // Assumption: We can always find message containing identifier
-    while (node?.type !== "message") {
-      node = node?.parent ?? null;
-      if (!node) {
-        return null;
-      }
-    }
-    const structName = node?.child(1)?.text;
-    if (!structName) {
-      return null;
-    }
-    const thatTree = this.getTree();
-    if (!thatTree) {
+    if (thisNode.type != "identifier") {
       return null;
     }
 
-    return [structName, fieldName];
+    let node = thisNode;
+    node = node.parent!;
+
+    if (node.type === "message_name") {
+      return handleCase1(thisNode);
+    } else if (node.type === "field") {
+      return handleCase2(thisNode);
+    } else if (node.type === "enum_name") {
+      return handleCase3(thisNode);
+    } else if (node.type === "enum_field") {
+      return handleCase4(thisNode);
+    }
+
+    console.debug("Unrecognised case");
+    return null;
   }
 
   getNode(route: Route) {
@@ -49,7 +114,62 @@ class ProtoWalker extends Walker {
     if (!route || !tree) {
       return null;
     }
-    const [structName, fieldName] = [route[0], route[1]];
+
+    let node = tree.rootNode;
+
+    let i = 0;
+    while (route[i]?.type === "message") {
+      let nodes;
+      if (i == 0) {
+        nodes = node.namedChildren;
+      } else {
+        nodes = node!.child(2)!.children.slice(1, -1); // message_body: skip { }
+      }
+      nodes = nodes.filter((node) => node?.type === "message");
+      node = nodes.find((node) => node?.child(1)?.text === route[i].name)!;
+      i += 1;
+    }
+
+    if (i === route.length) {
+      return node;
+    }
+    let nodes;
+    if (i == 0) {
+      nodes = node.namedChildren;
+    } else {
+      nodes = node!.child(2)!.children.slice(1, -1); // message_body: skip { }
+    }
+
+    if (route[i].type === "field") {
+      nodes = nodes.filter((node) => node?.type === "field");
+      nodes = nodes.map((node) => node?.child(2) ?? null);
+      return nodes.find((node) => node?.text === route[i].name) ?? null;
+    } else if (route[i].type === "enum") {
+      nodes = nodes.filter((node) => node?.type === "enum");
+      let node = nodes.find((node) => node?.child(1)?.text === route[i].name)!;
+
+      if (i + 1 == route.length) {
+        return nodes[0];
+      }
+
+      nodes = node!.children;
+      nodes = node.child(2)!.namedChildren;
+      nodes = nodes.filter((node) => node?.type === "enum_field");
+      nodes = nodes.filter(
+        (node) =>
+          node!.child(0)!.text.toLowerCase() === route[i + 1].name.toLowerCase()
+      );
+      const found = nodes[0];
+      return found;
+    }
+
+    if (route[0].type != "message") {
+      return null;
+    } // assertion failed
+    if (route[1].type != "field") {
+      return null;
+    } // assertion failed
+    const [structName, fieldName] = [route[0].name, route[1].name];
     const found = getProtoField(tree.rootNode, structName, fieldName);
     return found;
   }
